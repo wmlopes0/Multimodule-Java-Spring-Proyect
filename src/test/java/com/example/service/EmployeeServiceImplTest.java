fe85aad819e2c194e54e318f4749073bdee9cce5
Walter Martín Lopes <wmlopes0@gmail.com>
2024-03-19 16:21:37 +0100

    Generate EmployeeServiceImpl test with Mockito

--- src/test/java/com/example/service/EmployeeServiceImplTest.java
+++ src/test/java/com/example/service/EmployeeServiceImplTest.java
@@ -11,6 +11,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceImplTest {
@@ -19,19 +22,12 @@ class EmployeeServiceImplTest {
     @InjectMocks
     private EmployeeServiceImpl employeeService;
 
-//    @BeforeEach //Se ejecuta cada vez que se ejecuta un test
-//    void init() {
-//        repository = Mockito.mock(EmployeeRepository.class); //Instancia ficticia
-//        employeeService = new EmployeeServiceImpl(repository);
-//    }
-
-
     @Test
     void listAllEmployeesTest() {
-        Mockito.when(repository.findAll()).thenReturn(List.of(new Employee(1l, "Pepe"), new Employee(2l, "Walter")));
+        Mockito.when(repository.findAll()).thenReturn(List.of(new Employee(1L, "Pepe"), new Employee(2L, "Walter")));
 
         List<Employee> result = employeeService.listAllEmployees();
-        List<Employee> expected = List.of(new Employee(1l, "Pepe"), new Employee(2l, "Walter"));
+        List<Employee> expected = List.of(new Employee(1L, "Pepe"), new Employee(2L, "Walter"));
 
         Assertions.assertEquals(expected, result);
     }
@@ -41,5 +37,37 @@ class EmployeeServiceImplTest {
         Mockito.when(repository.findAll()).thenThrow(new RuntimeException("Error"));
         Assertions.assertThrows(RuntimeException.class, () -> employeeService.listAllEmployees());
     }
+
+    @Test
+    void addOrUpdateEmployeeTest() {
+        Employee newEmployee = new Employee(1L, "Walter");
+        employeeService.addEmployee(newEmployee);
+        verify(repository).save(newEmployee);
+    }
+
+    @Test
+    void deleteEmployeeByIdTest() {
+        Long id = 1L;
+        employeeService.deleteEmployeeById(id);
+        verify(repository).deleteById(1L);
+    }
+
+    @Test
+    void firstEmployeeContainsNameTest() {
+        String namePart = "Wal";
+        Employee expected = new Employee(1L, "Walter");
+        Mockito.when(repository.findFirstByNameContainingIgnoreCase(namePart)).thenReturn(Optional.of(expected));
+        Employee result = employeeService.firstEmployeeContainsName(namePart);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void firstEmployeeContainsNameNotFoundTest() {
+        String namePart = "Not found";
+        Mockito.when(repository.findFirstByNameContainingIgnoreCase(namePart)).thenReturn(Optional.empty());
+        Employee result = employeeService.firstEmployeeContainsName(namePart);
+        Assertions.assertNull(result);
+    }
+
 
 }
