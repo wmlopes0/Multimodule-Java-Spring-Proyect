df793eea7a02c698a8b5b7cea759423297f8e628
Walter Martín Lopes <wmlopes0@gmail.com>
2024-03-20 13:45:17 +0100

    Change EmployeeNameDetailsDTO to anemic class

--- src/main/java/com/example/model/EmployeeNameDetailsDTO.java
+++ src/main/java/com/example/model/EmployeeNameDetailsDTO.java
@@ -1,17 +1,18 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeNameDetailsDTO {
 
-    private final Long number;
-    private final String name;
-    private final int nameLength;
+    private Long number;
+    private String name;
+    private int nameLength;
 
-    public EmployeeNameDetailsDTO(Employee employee) {
-        this.number = employee.getNumber();
-        this.name = employee.getName().toUpperCase();
-        this.nameLength = name.length();
-    }
 }
