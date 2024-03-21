3b6163367fd0008710eb347ab25bc90da9084439
Walter Martín Lopes <wmlopes0@gmail.com>
2024-03-21 10:36:07 +0100

    Refactor and create EmployeeMapper

--- src/test/java/com/example/mapper/EmployeeMapperImplTest.java
+++ src/test/java/com/example/mapper/EmployeeMapperImplTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class EmployeeMapperImplTest {
 
-    private EmployeeMapperImpl employeeMapper = new EmployeeMapperImpl();
+    private final EmployeeMapperImpl employeeMapper = new EmployeeMapperImpl();
 
     @Test
     @DisplayName("Mapping Employee to EmployeeNameDetailsDTO correctly")
