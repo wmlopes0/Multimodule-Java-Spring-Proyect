54878691f0124abdc0de211f0f8d79605d24e9c3
Walter Martín Lopes <wmlopes0@gmail.com>
2024-03-27 14:22:44 +0100

    First commit

---
+++ walter-multimodule-training-domain/src/main/java/com/example/domain/Main.java
@@ -0,0 +1,11 @@
+package com.example.domain;
+
+import com.example.domain.vo.EmployeeNameVO;
+
+public class Main {
+
+  public static void main(String[] args) {
+    EmployeeNameVO employee = EmployeeNameVO.builder().name("Walter").build();
+  }
+
+}
---
+++ walter-multimodule-training-domain/src/main/java/com/example/domain/vo/EmployeeNameVO.java
@@ -0,0 +1,12 @@
+package com.example.domain.vo;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class EmployeeNameVO {
+
+  String name;
+
+}
