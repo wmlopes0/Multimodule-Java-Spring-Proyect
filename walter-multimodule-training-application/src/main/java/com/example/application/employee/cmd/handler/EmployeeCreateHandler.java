8d99cdb1dd97404003ceeb8ba6b5a02b243c56b4
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-03 09:57:30 +0200

    Refactor

--- walter-multimodule-training-application/src/main/java/com/example/application/employee/cmd/handler/EmployeeCreateHandler.java
+++ walter-multimodule-training-application/src/main/java/com/example/application/employee/cmd/handler/EmployeeCreateHandler.java
@@ -4,5 +4,6 @@ import com.example.application.employee.cmd.cmd.EmployeeCreateCmd;
 import com.example.domain.entity.Employee;
 
 public interface EmployeeCreateHandler {
+
   Employee addEmployee(EmployeeCreateCmd employeeCreateCmd);
 }
