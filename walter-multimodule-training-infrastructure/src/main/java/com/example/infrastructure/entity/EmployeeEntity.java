b34aa52f632a0fcf72b9c3006d4a1a66da62a306
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-08 10:55:48 +0200

    Refactor

--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/entity/EmployeeEntity.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/entity/EmployeeEntity.java
@@ -9,7 +9,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @AllArgsConstructor
