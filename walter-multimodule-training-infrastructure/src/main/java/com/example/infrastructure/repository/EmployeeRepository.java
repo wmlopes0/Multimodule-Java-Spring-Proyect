de818b47ed2226c9dd0d6899616f1fb9d7da364e
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-10 12:44:27 +0200

    Changed Repository to extend to the MongoRepository

--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
@@ -3,11 +3,11 @@ package com.example.infrastructure.repository;
 import java.util.Optional;
 
 import com.example.infrastructure.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+public interface EmployeeRepository extends MongoRepository<EmployeeEntity, Long> {
 
   Optional<EmployeeEntity> findFirstByNameContainingIgnoreCase(String namePart);
 }
