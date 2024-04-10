8c0b0ba901a7ec1a1ab00f6b05a36da3ac43c88d
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-10 16:05:44 +0200

    Refactor

--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
@@ -1,13 +1,9 @@
 package com.example.infrastructure.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import com.example.infrastructure.entity.EmployeeEntity;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,11 +11,9 @@ public interface EmployeeRepository extends MongoRepository<EmployeeEntity, Long
 
   Optional<EmployeeEntity> findFirstByNameContainingIgnoreCase(String namePart);
 
-  @Query(value = "{}", fields = "{number : 1}", sort = "{number : -1}")
-  List<EmployeeEntity> findTopByOrderByNumberDesc(Pageable pageable);
+  Optional<EmployeeEntity> findFirstByOrderByNumberDesc();
 
   default Long findMaxId() {
-    List<EmployeeEntity> entities = findTopByOrderByNumberDesc(PageRequest.of(0, 1));
-    return entities.isEmpty() ? null : entities.get(0).getNumber();
+    return findFirstByOrderByNumberDesc().map(EmployeeEntity::getNumber).orElse(null);
   }
 }
