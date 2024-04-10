50d1e0b4e4af59b536f28a928fbf5b41ecf26569
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-10 16:00:24 +0200

    Changed yml, EmployeeEntity and Repository

--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/repository/EmployeeRepository.java
@@ -1,13 +1,25 @@
 package com.example.infrastructure.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.infrastructure.entity.EmployeeEntity;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends MongoRepository<EmployeeEntity, Long> {
 
   Optional<EmployeeEntity> findFirstByNameContainingIgnoreCase(String namePart);
+
+  @Query(value = "{}", fields = "{number : 1}", sort = "{number : -1}")
+  List<EmployeeEntity> findTopByOrderByNumberDesc(Pageable pageable);
+
+  default Long findMaxId() {
+    List<EmployeeEntity> entities = findTopByOrderByNumberDesc(PageRequest.of(0, 1));
+    return entities.isEmpty() ? null : entities.get(0).getNumber();
+  }
 }
