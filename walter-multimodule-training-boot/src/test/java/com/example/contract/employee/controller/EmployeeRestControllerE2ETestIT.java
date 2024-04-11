789a4da1afedd06df5c5610ff0e3c08177f1a885
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-11 08:45:35 +0200

    Refactor

--- walter-multimodule-training-boot/src/test/java/com/example/contract/employee/controller/EmployeeRestControllerE2ETestIT.java
+++ walter-multimodule-training-boot/src/test/java/com/example/contract/employee/controller/EmployeeRestControllerE2ETestIT.java
@@ -22,7 +22,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = App.class, properties = {"spring.profiles.active = test"})
 @AutoConfigureMockMvc
