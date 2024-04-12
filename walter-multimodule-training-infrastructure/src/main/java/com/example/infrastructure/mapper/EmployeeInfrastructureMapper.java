694d2de43b32e68856a2c89461e4a7d48adc0e33
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-12 12:55:48 +0200

    Update EmployeeInfrastructureMapper

--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/mapper/EmployeeInfrastructureMapper.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/mapper/EmployeeInfrastructureMapper.java
@@ -1,10 +1,15 @@
 package com.example.infrastructure.mapper;
 
+import java.util.List;
+
 import com.example.domain.entity.Employee;
+import com.example.domain.entity.Gender;
 import com.example.domain.vo.EmployeeNameVO;
 import com.example.domain.vo.EmployeeNifVO;
 import com.example.domain.vo.EmployeeVO;
 import com.example.infrastructure.entity.EmployeeEntity;
+import com.example.infrastructure.entity.Phone;
+import com.example.infrastructure.entity.PhoneType;
 
 public interface EmployeeInfrastructureMapper {
 
@@ -12,7 +17,13 @@ public interface EmployeeInfrastructureMapper {
 
   EmployeeEntity mapToEntity(EmployeeNameVO employeeNameVO);
 
-  EmployeeEntity mapToEntity(EmployeeNifVO employeeUpdateVO);
+  EmployeeEntity mapToEntity(EmployeeNifVO employeeNifVO);
 
-  EmployeeEntity mapToEntity(EmployeeVO employeeUpdateVO);
+  EmployeeEntity mapToEntity(EmployeeVO employeeVO);
+
+  Gender mapToGender(int genderCode);
+
+  String extractPhoneWithTypeOfList(List<Phone> phones, PhoneType type);
+
+  Phone createPhone(String fullNumber, PhoneType type);
 }
--- walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/mapper/EmployeeInfrastructureMapperImpl.java
+++ walter-multimodule-training-infrastructure/src/main/java/com/example/infrastructure/mapper/EmployeeInfrastructureMapperImpl.java
@@ -1,7 +1,16 @@
 package com.example.infrastructure.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.domain.entity.Employee;
+import com.example.domain.entity.Gender;
+import com.example.domain.vo.EmployeeNameVO;
+import com.example.domain.vo.EmployeeNifVO;
+import com.example.domain.vo.EmployeeVO;
 import com.example.infrastructure.entity.EmployeeEntity;
+import com.example.infrastructure.entity.Phone;
+import com.example.infrastructure.entity.PhoneType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +19,14 @@ public class EmployeeInfrastructureMapperImpl implements EmployeeInfrastructureM
   @Override
   public Employee mapToDomain(EmployeeEntity employeeEntity) {
     return new Employee()
-        .setNumber(employeeEntity.getNumber())
-        .setName(employeeEntity.getName());
+        .setNif(employeeEntity.getNif())
+        .setName(employeeEntity.getName())
+        .setSurname(employeeEntity.getLastName())
+        .setBirthYear(employeeEntity.getBirthYear())
+        .setGender(mapToGender(employeeEntity.getGender()))
+        .setCompanyPhone(extractPhoneWithTypeOfList(employeeEntity.getPhones(), PhoneType.COMPANY))
+        .setPersonalPhone(extractPhoneWithTypeOfList(employeeEntity.getPhones(), PhoneType.PERSONAL))
+        .setEmail(employeeEntity.getEmail());
   }
 
   @Override
@@ -21,9 +36,65 @@ public class EmployeeInfrastructureMapperImpl implements EmployeeInfrastructureM
   }
 
   @Override
-  public EmployeeEntity mapToEntity(EmployeeUpdateVO employeeUpdateVO) {
+  public EmployeeEntity mapToEntity(EmployeeNifVO employeeNifVO) {
     return new EmployeeEntity()
-        .setNumber(employeeUpdateVO.getNumber())
-        .setName(employeeUpdateVO.getName());
+        .setNif(employeeNifVO.getNif());
   }
+
+  @Override
+  public EmployeeEntity mapToEntity(EmployeeVO employeeVO) {
+    List<Phone> phones = new ArrayList<>();
+
+    Phone companyPhone = createPhone(employeeVO.getCompanyPhone(), PhoneType.COMPANY);
+    if (companyPhone != null) {
+      phones.add(companyPhone);
+    }
+
+    Phone personalPhone = createPhone(employeeVO.getPersonalPhone(), PhoneType.PERSONAL);
+    if (personalPhone != null) {
+      phones.add(personalPhone);
+    }
+
+    return new EmployeeEntity()
+        .setNif(employeeVO.getNif())
+        .setName(employeeVO.getName())
+        .setLastName(employeeVO.getSurname())
+        .setBirthYear(employeeVO.getBirthYear())
+        .setGender(employeeVO.getGender().getCode())
+        .setPhones(phones)
+        .setEmail(employeeVO.getEmail());
+  }
+
+  @Override
+  public Gender mapToGender(int genderCode) {
+    switch (genderCode) {
+      case 1:
+        return Gender.MALE;
+      case 2:
+        return Gender.FEMALE;
+      default:
+        throw new IllegalArgumentException("Invalid gender code: " + genderCode);
+    }
+  }
+
+  @Override
+  public String extractPhoneWithTypeOfList(List<Phone> phones, PhoneType type) {
+    for (Phone phone : phones) {
+      if (phone.getType().equals(type)) {
+        return phone.getNumber();
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public Phone createPhone(String fullNumber, PhoneType type) {
+    if (fullNumber != null && !fullNumber.isEmpty()) {
+      String prefix = fullNumber.substring(0, 3);
+      String number = fullNumber.substring(3);
+      return new Phone(prefix, number, type);
+    }
+    return null;
+  }
+
 }
