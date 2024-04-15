88071b54f3b91441d60ecbdeba55082c224aa600
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-15 15:20:20 +0200

    Updated EmployeeContractMapperImpl tests

--- walter-multimodule-training-contract/src/test/java/com/example/contract/employee/mapper/EmployeeContractMapperImplTest.java
+++ walter-multimodule-training-contract/src/test/java/com/example/contract/employee/mapper/EmployeeContractMapperImplTest.java
@@ -1,33 +1,49 @@
 package com.example.contract.employee.mapper;
 
+import java.util.List;
+
 import com.example.application.employee.cmd.dto.EmployeeCreateCmd;
 import com.example.application.employee.cmd.dto.EmployeeDeleteCmd;
 import com.example.application.employee.cmd.dto.EmployeeUpdateCmd;
 import com.example.application.employee.query.dto.EmployeeByIdQuery;
 import com.example.application.employee.query.dto.EmployeeByNameQuery;
 import com.example.contract.employee.dto.EmployeeNameDTO;
+import com.example.contract.employee.dto.EmployeeNifDTO;
+import com.example.contract.employee.dto.EmployeeRequestDTO;
 import com.example.contract.employee.dto.EmployeeResponseDTO;
+import com.example.contract.employee.dto.PhoneDTO;
 import com.example.domain.entity.Employee;
+import com.example.domain.entity.Gender;
+import com.example.domain.entity.PhoneType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class EmployeeContractMapperImplTest {
 
   private final EmployeeContractMapper mapper = new EmployeeContractMapperImpl();
 
-  @ParameterizedTest
-  @CsvSource(value = {
-      "Walter",
-      "null"
-  }, nullValues = {"null"})
-  @DisplayName("Mapping EmployeeNameDTO to EmployeeCreateCmd correctly")
-  void mapToEmployeeCreateCmdTest(String name) {
-    EmployeeNameDTO employeeNameDTO = new EmployeeNameDTO(name);
-    EmployeeCreateCmd expected = new EmployeeCreateCmd(name);
-    EmployeeCreateCmd result = mapper.mapToEmployeeCreateCmd(employeeNameDTO);
+  @Test
+  @DisplayName("Mapping EmployeeRequestDTO to EmployeeCreateCmd correctly")
+  void mapToEmployeeCreateCmdTest() {
+    EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO()
+        .setNif("45134320V")
+        .setName("Walter")
+        .setSurname("Martín Lopes")
+        .setBirthYear(1998)
+        .setGender("Male")
+        .setPersonalPhone("+34722748406")
+        .setEmail("wmlopes0@gmail.com");
+    EmployeeCreateCmd expected = new EmployeeCreateCmd()
+        .setNif("45134320V")
+        .setName("Walter")
+        .setSurname("Martín Lopes")
+        .setBirthYear(1998)
+        .setGender("Male")
+        .setPersonalPhone("+34722748406")
+        .setEmail("wmlopes0@gmail.com");
+
+    EmployeeCreateCmd result = mapper.mapToEmployeeCreateCmd(employeeRequestDTO);
 
     Assertions.assertEquals(expected, result);
   }
@@ -35,75 +51,83 @@ class EmployeeContractMapperImplTest {
   @Test
   @DisplayName("Mapping id to EmployeeDeleteCmd correctly")
   void mapToEmployeeDeleteCmdTest() {
-    Long id = 1L;
-    EmployeeDeleteCmd expected = new EmployeeDeleteCmd(id);
-    EmployeeDeleteCmd result = mapper.mapToEmployeeDeleteCmd(id);
-
-    Assertions.assertEquals(expected, result);
-  }
-
-  @ParameterizedTest
-  @CsvSource(value = {
-      "1,Walter",
-      "2,null"
-  }, nullValues = {"null"})
-  @DisplayName("Mapping id and EmployeeNameDTO to EmployeeUpdateCmd correctly")
-  void mapToEmployeeUpdateCmdTest(Long id, String name) {
-    EmployeeNameDTO employeeNameDTO = new EmployeeNameDTO(name);
-    EmployeeUpdateCmd expected = new EmployeeUpdateCmd(id, name);
-    EmployeeUpdateCmd result = mapper.mapToEmployeeUpdateCmd(id, employeeNameDTO);
+    EmployeeNifDTO employeeNifDTO = new EmployeeNifDTO("45134320V");
+    EmployeeDeleteCmd expected = new EmployeeDeleteCmd(employeeNifDTO.getNif());
+    EmployeeDeleteCmd result = mapper.mapToEmployeeDeleteCmd(employeeNifDTO);
 
     Assertions.assertEquals(expected, result);
   }
 
   @Test
-  @DisplayName("Mapping id to EmployeeByIdQuery correctly")
+  @DisplayName("Mapping id and EmployeeRequestDTO to EmployeeUpdateCmd correctly")
+  void mapToEmployeeUpdateCmdTest() {
+    EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO()
+        .setNif("45134320V")
+        .setName("Walter")
+        .setSurname("Martín Lopes")
+        .setBirthYear(1998)
+        .setGender("Male")
+        .setPersonalPhone("+34722748406")
+        .setEmail("wmlopes0@gmail.com");
+    EmployeeUpdateCmd expected = new EmployeeUpdateCmd()
+        .setNif("45134320V")
+        .setName("Walter")
+        .setSurname("Martín Lopes")
+        .setBirthYear(1998)
+        .setGender("Male")
+        .setPersonalPhone("+34722748406")
+        .setEmail("wmlopes0@gmail.com");
+
+    EmployeeUpdateCmd result = mapper.mapToEmployeeUpdateCmd(employeeRequestDTO);
+    Assertions.assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Mapping EmployeeNifDTO to EmployeeByIdQuery correctly")
   void mapToEmployeeByIdQueryTest() {
-    Long id = 1L;
-    EmployeeByIdQuery expected = new EmployeeByIdQuery(id);
-    EmployeeByIdQuery result = mapper.mapToEmployeeByIdQuery(id);
+    EmployeeNifDTO employeeNifDTO = new EmployeeNifDTO("45134320V");
+    EmployeeByIdQuery expected = new EmployeeByIdQuery(employeeNifDTO.getNif());
+    EmployeeByIdQuery result = mapper.mapToEmployeeByIdQuery(employeeNifDTO);
 
     Assertions.assertEquals(expected, result);
   }
 
-  @ParameterizedTest
-  @CsvSource(value = {
-      "Walter",
-      "null"
-  }, nullValues = {"null"})
-  @DisplayName("Mapping name to EmployeeByNameQuery correctly")
-  void mapToEmployeeByNameQueryTest(String name) {
-    EmployeeByNameQuery expected = new EmployeeByNameQuery(name);
-    EmployeeByNameQuery result = mapper.mapToEmployeeByNameQuery(name);
+  @Test
+  @DisplayName("Mapping EmployeeNameDTO to EmployeeByNameQuery correctly")
+  void mapToEmployeeByNameQueryTest() {
+    EmployeeNameDTO employeeNameDTO = new EmployeeNameDTO("Walter");
+    EmployeeByNameQuery expected = new EmployeeByNameQuery(employeeNameDTO.getName());
+    EmployeeByNameQuery result = mapper.mapToEmployeeByNameQuery(employeeNameDTO);
 
     Assertions.assertEquals(expected, result);
   }
 
-  @ParameterizedTest
-  @CsvSource(value = {
-      "Walter,WALTER,6",
-      "null,null,0"
-  }, nullValues = {"null"})
-  @DisplayName("Mapping Employee to EmployeeNameDetailsDTO correctly")
-  void mapToDetailsDTOTest(String name, String expectedName, int expectedLength) {
-    Employee employee = new Employee(1L, name);
-    EmployeeNameDetailsDTO expected = new EmployeeNameDetailsDTO(1L, expectedName, expectedLength);
-
-    EmployeeNameDetailsDTO result = mapper.mapToDetailsDTO(employee);
-    Assertions.assertEquals(expected, result);
-  }
-
-  @ParameterizedTest
-  @CsvSource(value = {
-      "1,Walter",
-      "1,null"
-  }, nullValues = {"null"})
+  @Test
   @DisplayName("Mapping Employee to EmployeeResponseDTO correctly")
-  void mapToResponseDTOTest(Long number, String name) {
-    Employee employee = new Employee(number, name);
-    EmployeeResponseDTO expected = new EmployeeResponseDTO(number, name);
-    EmployeeResponseDTO result = mapper.mapToResponseDTO(employee);
+  void mapToResponseDTOTest() {
+    Employee employee = new Employee()
+        .setNif("45134320V")
+        .setName("Walter")
+        .setSurname("Martín Lopes")
+        .setBirthYear(1998)
+        .setGender(Gender.MALE)
+        .setPersonalPhone("+34722748406")
+        .setCompanyPhone("+34676615106")
+        .setEmail("wmlopes0@gmail.com");
 
+    EmployeeResponseDTO expected = new EmployeeResponseDTO()
+        .setNif("45134320V")
+        .setCompleteName("Martín Lopes, Walter")
+        .setBirthYear(1998)
+        .setAge(26)
+        .setAdult(true)
+        .setGender("Male")
+        .setPhones(List.of(
+            new PhoneDTO("+34722748406", PhoneType.PERSONAL.name()),
+            new PhoneDTO("+34676615106", PhoneType.COMPANY.name())))
+        .setEmail("wmlopes0@gmail.com");
+
+    EmployeeResponseDTO result = mapper.mapToResponseDTO(employee);
     Assertions.assertEquals(expected, result);
   }
 }
