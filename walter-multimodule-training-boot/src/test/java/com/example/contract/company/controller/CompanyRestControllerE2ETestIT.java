48df5ff1ef46f606bce0cba3b929ca9f0f647d36
Walter Martín Lopes <wmlopes0@gmail.com>
2024-04-29 13:51:38 +0200

    Generated E2E tests

---
+++ walter-multimodule-training-boot/src/test/java/com/example/contract/company/controller/CompanyRestControllerE2ETestIT.java
@@ -0,0 +1,272 @@
+package com.example.contract.company.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import com.example.boot.app.App;
+import com.example.infrastructure.entity.CompanyEntity;
+import com.example.infrastructure.entity.EmployeeEntity;
+import com.example.infrastructure.repository.CompanyRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = App.class, properties = {"spring.profiles.active = test"})
+@AutoConfigureMockMvc
+class CompanyRestControllerE2ETestIT {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @BeforeEach
+  void init() {
+    companyRepository.deleteAll();
+  }
+
+  @ParameterizedTest
+  @MethodSource("listCompaniesParameters")
+  @DisplayName("List companies return correctly list")
+  void listCompaniesTest(CompanyEntity companyEntity1, CompanyEntity companyEntity2, String expected) throws Exception {
+    companyRepository.save(companyEntity1);
+    companyRepository.save(companyEntity2);
+    mockMvc.perform(get("/companies/"))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expected, false));
+  }
+
+  private static Stream<Arguments> listCompaniesParameters() {
+    return Stream.of(
+        Arguments.of(
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company1 S.L")
+                .setEmployees(List.of()),
+            new CompanyEntity()
+                .setCif("U52304771")
+                .setName("Company2 S.L")
+                .setEmployees(List.of()),
+            """
+                [
+                  {
+                    "cif": "B86017472",
+                    "name": "Company1 S.L",
+                    "employees": []
+                  },
+                  {
+                    "cif": "U52304771",
+                    "name": "Company2 S.L",
+                    "employees": []
+                  }
+                ]"""
+        )
+    );
+  }
+
+  @Test
+  @DisplayName("When list companies is empty return correctly list")
+  void listCompaniesEmptyTest() throws Exception {
+    String expected = """
+        []""";
+
+    mockMvc.perform(get("/companies/"))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expected, true));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getCompanyByIdParameters")
+  @DisplayName("Get company by CIF returns company and 200 response correctly")
+  void getCompanyByIdTest(String cif, CompanyEntity companyEntity, String expected) throws Exception {
+    companyRepository.save(companyEntity);
+    mockMvc.perform(get("/companies/{cif}", cif))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expected, false));
+  }
+
+  private static Stream<Arguments> getCompanyByIdParameters() {
+    return Stream.of(
+        Arguments.of(
+            "B86017472",
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company1 S.L")
+                .setEmployees(List.of()),
+            """
+                {
+                    "cif": "B86017472",
+                    "name": "Company1 S.L",
+                    "employees": []
+                  }"""
+        )
+    );
+  }
+
+  @Test
+  @DisplayName("Get company by CIF not found returns 404 response")
+  void getCompanyByIdNotFoundTest() throws Exception {
+    String expected = "";
+    String cif = "B86017472";
+
+    mockMvc.perform(get("/companies/{cif}", cif))
+        .andExpect(status().isNotFound())
+        .andExpect(content().string(expected));
+  }
+
+  @ParameterizedTest
+  @MethodSource("newCompanyParameters")
+  @DisplayName("Add new company returns 201 response")
+  void newCompanyTest(String cif, String jsonContent, String expected, CompanyEntity entityExpected) throws Exception {
+    mockMvc.perform(post("/companies/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonContent))
+        .andExpect(status().isCreated())
+        .andExpect(content().json(expected, false));
+
+    Optional<CompanyEntity> fetchExpected = Optional.of(entityExpected);
+    Optional<CompanyEntity> fetchResult = companyRepository.findById(cif);
+
+    Assertions.assertEquals(fetchExpected, fetchResult);
+  }
+
+  private static Stream<Arguments> newCompanyParameters() {
+    return Stream.of(
+        Arguments.of(
+            "B86017472",
+            """
+                {
+                  "cif": "B86017472",
+                  "name": "Company1 S.L"
+                }
+                """,
+            """
+                {
+                  "cif": "B86017472",
+                  "name": "Company1 S.L",
+                  "employees": []
+                }
+                """,
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company1 S.L")
+                .setEmployees(List.of())
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("updateCompanyByIdParameters")
+  @DisplayName("Update company by CIF successfully returns 200 code response")
+  void updateCompanyByIdTest(String cif, CompanyEntity companyEntity, String jsonContent, String expected,
+      CompanyEntity entityExpected) throws Exception {
+    companyRepository.save(companyEntity);
+
+    mockMvc.perform(put("/companies/{cif}", cif)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonContent))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expected, false));
+
+    Optional<CompanyEntity> fetchExpected = Optional.of(entityExpected);
+    Optional<CompanyEntity> fetchResult = companyRepository.findById(cif);
+
+    Assertions.assertEquals(fetchExpected, fetchResult);
+  }
+
+  private static Stream<Arguments> updateCompanyByIdParameters() {
+    return Stream.of(
+        Arguments.of(
+            "B86017472",
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company1 S.L")
+                .setEmployees(List.of()),
+            """
+                {
+                  "cif": "B86017472",
+                  "name": "Company Name Changed"
+                }
+                """,
+            """
+                {
+                  "cif": "B86017472",
+                  "name": "Company Name Changed",
+                  "employees": []
+                }
+                """,
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company Name Changed")
+                .setEmployees(List.of())
+        )
+    );
+  }
+
+  @Test
+  @DisplayName("Update company by CIF not found returns 404 code response")
+  void updateCompanyByIdNotFoundTest() throws Exception {
+    String cif = "B86017472";
+    String jsonContent = """
+        {
+          "cif": "B86017472",
+          "name": "Company Name Changed"
+        }
+        """;
+    String expected = """
+        {
+          "error":"Internal Server Error",
+          "message":"No company found with that ID."
+        }
+        """;
+    mockMvc.perform(put("/companies/{cif}", cif)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonContent))
+        .andExpect(status().isInternalServerError())
+        .andExpect(content().json(expected, false));
+  }
+
+  @Test
+  @DisplayName("Deleted employee by CIF successfully returns 200 code response")
+  void deleteCompanyByIdTest() throws Exception {
+    String cif = "B86017472";
+    companyRepository.save(new CompanyEntity()
+        .setCif("B86017472")
+        .setName("Company")
+        .setEmployees(List.of()));
+
+    mockMvc.perform(delete("/companies/{cif}", cif))
+        .andExpect(status().isOk());
+
+    Optional<CompanyEntity> fetchExpected = Optional.empty();
+    Optional<CompanyEntity> fetchResult = companyRepository.findById(cif);
+
+    Assertions.assertEquals(fetchExpected, fetchResult);
+  }
+
+  @Test
+  @DisplayName("Deleted employee by CIF failed returns 404 code response.")
+  void deleteCompanyByIdNotFoundTest() throws Exception {
+    String cif = "B86017472";
+    mockMvc.perform(delete("/companies/{cif}", cif))
+        .andExpect(status().isNotFound());
+  }
+}
--- walter-multimodule-training-boot/src/test/java/com/example/contract/employee/controller/EmployeeRestControllerE2ETestIT.java
+++ walter-multimodule-training-boot/src/test/java/com/example/contract/employee/controller/EmployeeRestControllerE2ETestIT.java
@@ -15,8 +15,10 @@ import java.util.stream.Stream;
 import com.example.boot.app.App;
 import com.example.domain.entity.Gender;
 import com.example.domain.entity.PhoneType;
+import com.example.infrastructure.entity.CompanyEntity;
 import com.example.infrastructure.entity.EmployeeEntity;
 import com.example.infrastructure.entity.PhoneEntity;
+import com.example.infrastructure.repository.CompanyRepository;
 import com.example.infrastructure.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,19 +42,23 @@ class EmployeeRestControllerE2ETestIT {
   private MockMvc mockMvc;
 
   @Autowired
-  private EmployeeRepository repository;
+  private EmployeeRepository employeeRepository;
+
+  @Autowired
+  private CompanyRepository companyRepository;
 
   @BeforeEach
   void init() {
-    repository.deleteAll();
+    employeeRepository.deleteAll();
+    companyRepository.deleteAll();
   }
 
   @ParameterizedTest
   @MethodSource("listEmployeesParameters")
   @DisplayName("List employees return correctly list")
   void listEmployeesTest(EmployeeEntity employeeEntity1, EmployeeEntity employeeEntity2, String expected) throws Exception {
-    repository.save(employeeEntity1);
-    repository.save(employeeEntity2);
+    employeeRepository.save(employeeEntity1);
+    employeeRepository.save(employeeEntity2);
 
     mockMvc.perform(get("/employees/"))
         .andExpect(status().isOk())
@@ -130,7 +136,7 @@ class EmployeeRestControllerE2ETestIT {
   @MethodSource("getEmployeeByIdParameters")
   @DisplayName("Get employee by ID returns employee and 200 response correctly")
   void getEmployeeByIdTest(String nif, EmployeeEntity employeeEntity, String expected) throws Exception {
-    repository.save(employeeEntity);
+    employeeRepository.save(employeeEntity);
 
     mockMvc.perform(get("/employees/{nif}", nif))
         .andExpect(status().isOk())
@@ -202,7 +208,7 @@ class EmployeeRestControllerE2ETestIT {
   @MethodSource("getEmployeeByNameParameters")
   @DisplayName("Get employee by name returns employee and 200 response correctly")
   void getEmployeeByNameTest(String name, EmployeeEntity employeeEntity, String expected) throws Exception {
-    repository.save(employeeEntity);
+    employeeRepository.save(employeeEntity);
 
     mockMvc.perform(get("/employees/name/{name}", name))
         .andExpect(status().isOk())
@@ -262,7 +268,7 @@ class EmployeeRestControllerE2ETestIT {
         .andExpect(content().json(expected, false));
 
     Optional<EmployeeEntity> fetchExpected = Optional.of(entityExpected);
-    Optional<EmployeeEntity> fetchResult = repository.findById(nif);
+    Optional<EmployeeEntity> fetchResult = employeeRepository.findById(nif);
 
     Assertions.assertEquals(fetchExpected, fetchResult);
   }
@@ -368,7 +374,7 @@ class EmployeeRestControllerE2ETestIT {
   @DisplayName("Update employee by ID successfully returns 200 code response")
   void updateEmployeeByIdTest(String nif, EmployeeEntity employeeEntity, String jsonContent, String expected, EmployeeEntity entityExpected)
       throws Exception {
-    repository.save(employeeEntity);
+    employeeRepository.save(employeeEntity);
 
     mockMvc.perform(put("/employees/{nif}", nif)
             .contentType(MediaType.APPLICATION_JSON)
@@ -377,7 +383,7 @@ class EmployeeRestControllerE2ETestIT {
         .andExpect(content().json(expected, false));
 
     Optional<EmployeeEntity> fetchExpected = Optional.of(entityExpected);
-    Optional<EmployeeEntity> fetchResult = repository.findById(nif);
+    Optional<EmployeeEntity> fetchResult = employeeRepository.findById(nif);
 
     Assertions.assertEquals(fetchExpected, fetchResult);
   }
@@ -490,12 +496,188 @@ class EmployeeRestControllerE2ETestIT {
         .andExpect(content().string(""));
   }
 
+  @ParameterizedTest
+  @MethodSource("addEmployeeToCompanyParameters")
+  @DisplayName("Add employee to company successfully returns 200 code response")
+  void addEmployeeToCompany(EmployeeEntity employeeEntity, CompanyEntity companyEntity, String expected,
+      EmployeeEntity employeeEntityExpected,
+      CompanyEntity companyEntityExpected) throws Exception {
+
+    employeeRepository.save(employeeEntity);
+    companyRepository.save(companyEntity);
+
+    String nif = "45134320V";
+    String jsonContent = """
+        {
+               "cif": "B86017472"
+              }""";
+
+    mockMvc.perform(put("/addToCompany/{nif}", nif)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonContent))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().json(expected, false));
+
+    Optional<EmployeeEntity> fetchEmployeeExpected = Optional.of(employeeEntityExpected);
+    Optional<EmployeeEntity> fetchEmployeeResult = employeeRepository.findById(nif);
+
+    Assertions.assertEquals(fetchEmployeeExpected, fetchEmployeeResult);
+
+    Optional<CompanyEntity> fetchCompanyExpected = Optional.of(companyEntityExpected);
+    Optional<CompanyEntity> fetchCompanyResult = companyRepository.findById("B86017472");
+
+    Assertions.assertEquals(fetchCompanyExpected, fetchCompanyResult);
+  }
+
+  private static Stream<Arguments> addEmployeeToCompanyParameters() {
+    return Stream.of(
+        Arguments.of(
+            new EmployeeEntity()
+                .setNif("45134320V")
+                .setName("Walter")
+                .setLastName("Martín Lopes")
+                .setBirthYear(1998)
+                .setGender(Gender.MALE.getCode())
+                .setCompany(null)
+                .setPhones(List.of(
+                    new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                .setEmail("wmlopes0@gmail.com"),
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company2 S.L")
+                .setEmployees(List.of()),
+            """
+                {
+                  "cif": "B86017472",
+                  "name": "Company2 S.L",
+                  "employees":[
+                  {
+                  "nif": "45134320V",
+                  "name": "Walter",
+                  "surname":"Martín Lopes"
+                  "birthYear":"1998",
+                  "gender": "MALE",
+                  "personalPhone": "+34722748406,",
+                  "company": "B86017472",
+                  "email": "wmlopes0@gmail.com"
+                  }
+                  ]
+                }
+                """,
+            new EmployeeEntity()
+                .setNif("45134320V")
+                .setName("Walter")
+                .setLastName("Martín Lopes")
+                .setBirthYear(1998)
+                .setGender(Gender.MALE.getCode())
+                .setCompany("B86017472")
+                .setPhones(List.of(
+                    new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                .setEmail("wmlopes0@gmail.com"),
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company2 S.L")
+                .setEmployees(List.of(
+                        new EmployeeEntity()
+                            .setNif("45134320V")
+                            .setName("Walter")
+                            .setLastName("Martín Lopes")
+                            .setBirthYear(1998)
+                            .setGender(Gender.MALE.getCode())
+                            .setCompany("B86017472")
+                            .setPhones(List.of(
+                                new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                            .setEmail("wmlopes0@gmail.com")
+                    )
+                )
+        )
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("removeEmployeeFromCompanyParameters")
+  @DisplayName("Remove employee from company successfully returns 200 code response")
+  void removeEmployeeFromCompany(EmployeeEntity employeeEntity, CompanyEntity companyEntity, EmployeeEntity employeeEntityExpected,
+      CompanyEntity companyEntityExpected) throws Exception {
+    employeeRepository.save(employeeEntity);
+    companyRepository.save(companyEntity);
+
+    String nif = "45134320V";
+    String jsonContent = """
+        {
+               "cif": "B86017472"
+              }""";
+
+    mockMvc.perform(put("/removeToCompany/{nif}", nif)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonContent))
+        .andDo(print())
+        .andExpect(status().isOk());
+
+    Optional<EmployeeEntity> fetchEmployeeExpected = Optional.of(employeeEntityExpected);
+    Optional<EmployeeEntity> fetchEmployeeResult = employeeRepository.findById(nif);
+
+    Assertions.assertEquals(fetchEmployeeExpected, fetchEmployeeResult);
+
+    Optional<CompanyEntity> fetchCompanyExpected = Optional.of(companyEntityExpected);
+    Optional<CompanyEntity> fetchCompanyResult = companyRepository.findById("B86017472");
+
+    Assertions.assertEquals(fetchCompanyExpected, fetchCompanyResult);
+  }
+
+  private static Stream<Arguments> removeEmployeeFromCompanyParameters() {
+    return Stream.of(
+        Arguments.of(
+            new EmployeeEntity()
+                .setNif("45134320V")
+                .setName("Walter")
+                .setLastName("Martín Lopes")
+                .setBirthYear(1998)
+                .setGender(Gender.MALE.getCode())
+                .setCompany("B86017472")
+                .setPhones(List.of(
+                    new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                .setEmail("wmlopes0@gmail.com"),
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company2 S.L")
+                .setEmployees(List.of(
+                        new EmployeeEntity()
+                            .setNif("45134320V")
+                            .setName("Walter")
+                            .setLastName("Martín Lopes")
+                            .setBirthYear(1998)
+                            .setGender(Gender.MALE.getCode())
+                            .setCompany("B86017472")
+                            .setPhones(List.of(
+                                new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                            .setEmail("wmlopes0@gmail.com")
+                    )
+                ),
+            new EmployeeEntity()
+                .setNif("45134320V")
+                .setName("Walter")
+                .setLastName("Martín Lopes")
+                .setBirthYear(1998)
+                .setGender(Gender.MALE.getCode())
+                .setCompany(null)
+                .setPhones(List.of(
+                    new PhoneEntity("+34", "722748406", PhoneType.PERSONAL)))
+                .setEmail("wmlopes0@gmail.com"),
+            new CompanyEntity()
+                .setCif("B86017472")
+                .setName("Company2 S.L")
+                .setEmployees(List.of()))
+    );
+  }
+
   @Test
   @DisplayName("Deleted employee by ID successfully returns 200 code response")
   void deleteEmployeeByIdTest() throws Exception {
     String nif = "45134320V";
 
-    repository.save(new EmployeeEntity()
+    employeeRepository.save(new EmployeeEntity()
         .setNif(nif)
         .setName("Walter")
         .setLastName("Martín Lopes")
@@ -509,7 +691,7 @@ class EmployeeRestControllerE2ETestIT {
         .andExpect(status().isOk());
 
     Optional<EmployeeEntity> fetchExpected = Optional.empty();
-    Optional<EmployeeEntity> fetchResult = repository.findById(nif);
+    Optional<EmployeeEntity> fetchResult = employeeRepository.findById(nif);
 
     Assertions.assertEquals(fetchExpected, fetchResult);
   }
